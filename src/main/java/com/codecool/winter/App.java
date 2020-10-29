7cdfd49ab7b73ab80184c021159b3f3a46571f05
MateuszGolda <mateusz.golda1993@gmail.com>
2020-10-29 13:12:01 +0100

    refactor: remove redundant exception from method declaration

--- src/main/java/com/codecool/winter/App.java
+++ src/main/java/com/codecool/winter/App.java
@@ -3,7 +3,7 @@ package com.codecool.winter;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException, NoSuchMethodException {
+    public static void main(String[] args) throws IOException {
         var winter = new Winter();
         winter.registerHandlers(ExampleHandler.class, HttpRequestHandler.class);
         winter.run();
